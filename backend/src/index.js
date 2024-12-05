cd47715ae19bb893e6da55a989094ae47235696c
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2024-12-05 13:55:38 +0530

    Message Router and Controller is Done

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.route.js';
+import messageRouter from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
 const app = express();
 
@@ -10,6 +11,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/message', messageRouter)
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
