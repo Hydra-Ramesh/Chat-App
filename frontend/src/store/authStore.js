b477036c85e5a627036661e2a4437081cca38540
Ramesh Das <rameshdas@Rameshs-MacBook-Air.local>
2024-12-06 16:40:32 +0530

    Create Profile Page

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -57,5 +57,17 @@ export const useAuthStore = create((set) => ({
     }finally{
       set({isLoggingIn: false});
     }
+  },
+  updateProfile: async (data) => {
+     set({isUpdatingProfile: true});
+     try{
+       const res = await axiosInstance.put(`/auth/update-profile`, data);
+       set({authUser: res.data});
+       toast.success("Profile Updated Successfully");
+     }catch(error){
+       toast.error(error.response.data.message);
+     }finally{
+       set({isUpdatingProfile: false});
+     }
   }
 }));
